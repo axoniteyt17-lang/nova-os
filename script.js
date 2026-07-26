6ed406b480c1702d8edb8e167c8c54fbd56d1dda
axoniteyt17-lang <your-github-email@example.com>
2026-07-26 17:30:42 +0100

    fix loading issue

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// Open Apps
+
 function openApp(app){
 
     document.getElementById(app).style.display="block";
 
 }
+
 
 
 function closeApp(app){
@@ -12,23 +15,35 @@ function closeApp(app){
 }
 
 
+
+
 // Clock
 
 function updateClock(){
 
-    let time = new Date();
+    let clock = document.getElementById("clock");
 
-    document.getElementById("clock").textContent =
-    time.toLocaleTimeString([], {
-        hour:"2-digit",
-        minute:"2-digit"
-    });
+    if(clock){
+
+        let time = new Date();
+
+        clock.textContent =
+        time.toLocaleTimeString([], {
+            hour:"2-digit",
+            minute:"2-digit"
+        });
+
+    }
 
 }
+
 
 setInterval(updateClock,1000);
 
 updateClock();
+
+
+
 
 
 
@@ -37,72 +52,86 @@ updateClock();
 function changeWallpaper(){
 
     let colours=[
+
         "linear-gradient(135deg,#2563eb,#7c3aed)",
+
         "linear-gradient(135deg,#10b981,#06b6d4)",
+
         "linear-gradient(135deg,#ef4444,#f59e0b)"
+
     ];
+
 
     document.getElementById("desktop").style.background =
     colours[Math.floor(Math.random()*colours.length)];
 
 }
-// Boot screen
 
-window.onload = () => {
+
+
+
+
+
+
+// Boot sequence
+
+window.addEventListener("load", () => {
+
 
     setTimeout(() => {
 
-        document.getElementById("boot").style.display = "none";
 
-        document.getElementById("login").style.display = "flex";
-
-    }, 2500);
-
-};
-
-// Startup sequence
-
-window.onload = () => {
-
-    setTimeout(() => {
-
-        // Hide boot screen
-        document.getElementById("boot").style.display = "none";
-
-        // Show login
-        document.getElementById("login").style.display = "flex";
-
-    }, 2500);
-
-};
+        document.getElementById("boot").style.display="none";
 
 
-// Login button
+        document.getElementById("login").style.display="flex";
+
+
+    },2500);
+
+
+});
+
+
+
+
+
+
+
+
+// Login
 
 function login(event){
 
     event.preventDefault();
 
+
     let username =
     document.getElementById("username").value;
+
 
     let password =
     document.getElementById("password").value;
 
 
+
     if(username !== "" && password !== ""){
 
-        // Hide login
-        document.getElementById("login").style.display = "none";
 
-        // Show desktop
-        document.getElementById("desktop").style.display = "block";
+        document.getElementById("login").style.display="none";
 
-    } 
-    else {
+
+        document.getElementById("desktop").style.display="block";
+
+
+    }
+
+    else{
+
 
         document.getElementById("error").textContent =
         "Please complete all fields.";
+
 
     }
 
